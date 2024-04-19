bc122df189f79db9d87a4e3ed53420804328d032
ulurn-av <ulurn@yandex.ru>
2024-04-19 16:18:05 +0700

    добавил typing для функции, обработал исключение, ошибка в слове контейнер, теперь программа не падает при неправильном считывании штрих кода

--- src/scanner/scanner.py
+++ src/scanner/scanner.py
@@ -2,16 +2,18 @@ import serial
 from src.constants import SCANNER_PORT
 
 
-def barcode_scanner():
+def barcode_scanner() -> int | None:
     ser = serial.Serial(SCANNER_PORT, 9600, timeout=0.1)
     s = ser.readline()
     while s == b'':
         s = ser.readline()
     ser.close()
-
-    # Тут при декодировании рухнет с ошибкой, если считывать не штрих код, а QR допустим (сканнер может все подряд считывать)
     decoded = s.decode('utf-8').strip('\r')
-    return int(decoded)
+
+    try:
+        return int(decoded)
+    except ValueError:
+        return None
 
 
 if __name__ == '__main__':
