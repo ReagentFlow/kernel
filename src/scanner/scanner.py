c96c11614707c432b3aff16ddc3a8040dc922e2c
ulurn-av <ulurn@yandex.ru>
2024-04-19 16:31:14 +0700

    typing

--- src/scanner/scanner.py
+++ src/scanner/scanner.py
@@ -1,8 +1,9 @@
 import serial
 from src.constants import SCANNER_PORT
+from typing import Union
 
 
-def barcode_scanner() -> int | None:
+def barcode_scanner() -> Union[int, None]:
     ser = serial.Serial(SCANNER_PORT, 9600, timeout=0.1)
     s = ser.readline()
     while s == b'':
