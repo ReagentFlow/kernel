4b56ab25f39d46d211d80d252893d0ce4afdbb36
ulurn-av <ulurn@yandex.ru>
2024-04-26 19:16:41 +0700

    add field with datetime and some functionality with this field

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from scanner import barcode_scanner
 from scales import getting_weight
 from fire_db import FireDataBase
@@ -6,17 +8,16 @@ from constants import KEY, COLLECTION
 
 
 def _updating() -> None:
-    data = ContainerData(name=str(key), weight=weight, barcode_id=key, container_id=0, density=0)
-    containter = Container(key=key, data=data)
-    database.update(COLLECTION, containter)
+    data = ContainerData(name=str(key), weight=weight, barcode_id=key, container_id=0, density=0, date=datetime.now())
+    container = Container(key=key, data=data)
+    database.update(COLLECTION, container)
 
 
 def _creating() -> None:
-    containter_id = len(database.list(collection=COLLECTION)) + 1
-    data = ContainerData(name=str(key), weight=weight, barcode_id=key, container_id=containter_id,
-                         density=0)
-    containter = Container(key=key, data=data)
-    database.create(COLLECTION, containter)
+    container_id = len(database.list(collection=COLLECTION)) + 1
+    data = ContainerData(name=str(key), weight=weight, barcode_id=key, container_id=container_id, density=0, date=datetime.now())
+    container = Container(key=key, data=data)
+    database.create(COLLECTION, container)
 
 
 if __name__ == "__main__":
@@ -25,7 +26,6 @@ if __name__ == "__main__":
         key = barcode_scanner()
         if key:
             weight = getting_weight()
-
             if database.get(str(key), COLLECTION):
                 _updating()
                 print('update')
