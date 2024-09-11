8b61bba751b784ee09e65ba8beccf7d3144fcfb7
eegii <a.mongush3@g.nsu.ru>
2024-09-11 19:30:01 +0700

    change main, apiconnection

--- src/main.py
+++ src/main.py
@@ -1,55 +1,39 @@
 from datetime import datetime
+import RPI.GPIO as GPIO
+from RPLCD.gpio import CharLCD
+from time import sleep
 
 from scanner import barcode_scanner
 from scales import getting_weight
-from fire_db import FireDataBase
-from models import Container, ContainerData
 from constants import KEY, COLLECTION
 from button import Button, PinOUT, PinIN
-from display import MainWindow
 from display.display_melton import Display
+from connection.base import APIConnection
 
 
-def _updating(key: int, weight: int) -> None:
-    data = ContainerData(name=str(key), weight=weight, barcode_id=key,
-                         container_id=0, density=0, date=datetime.now())
-    container = Container(key=key, data=data)
-    database.update(COLLECTION, container)
 
 
-def _creating(key: int, weight: int) -> None:
-    container_id = len(database.list(collection=COLLECTION)) + 1
-    data = ContainerData(name=str(key), weight=weight, barcode_id=key,
-                         container_id=container_id, density=0, date=datetime.now())
-    container = Container(key=key, data=data)
-    database.create(COLLECTION, container)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+def scanner_check():
+    pass
+
+def scales_check():
+    pass
 
 
-def main(window_instance: MainWindow) -> None:
-
-    key = barcode_scanner()
-    window_instance.update_info(barcode=key)
-
-    if key:
-        weight = getting_weight()
-        window_instance.update_info(barcode=key, weight=weight)
-
-        if database.get(str(key), COLLECTION):
-            _updating(key, weight)
-            print('update')
-        else:
-            _creating(key, weight)
-            print('create')
-    else:
-        print("Видимо вы считали QR код, а не штрих код!")
+def main():
+    pass
 
 
 if __name__ == "__main__":
-    database = FireDataBase(KEY)
-    pin_in = PinIN(40)
-    pin_out = PinOUT(38)
-    button = Button(pin_in, pin_out)
-    window = MainWindow()
+    # pin_in = PinIN(40)
+    # pin_out = PinOUT(38)
+    # button = Button(pin_in, pin_out)
+
+    display = Display()
+    display.clear()
 
     try:
         while True:
