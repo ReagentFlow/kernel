50beb7c7c4c63f6fc36e7f9f52cf607747e54cb7
eegii <a.mongush3@g.nsu.ru>
2024-09-17 15:43:23 +0700

    change class

--- src/connection/base.py
+++ src/connection/base.py
@@ -1,37 +1,41 @@
 import requests
 
 class APIConnection:
-    def __init__(self, API_url):
-        self.url = API_url
+    def __init__(self, api_url="http://127.0.0.1"):
+        self.url = api_url
 
     def get_item(self, item_id):
+        """Получение элемента по ID через запрос GET"""
         try:
             response = requests.get(f"{self.url}/item/{item_id}")
-            response.raise_for_status()
-            print('success')
-            return response.json()
-        except requests.exceptions.HTTPError as err:
-            if response.status_code == 404:
-                print('id error')
-            else:
-                print(f"HTTP error occurred: {err}")
-
-    def create_item(self, data):
-        try:
-            response = requests.post(f"{self.url}/item", json=data)
-            response.raise_for_status()
-            return response.json()
+            response.raise_for_status()  # Проверка на успешный ответ
+            return response.json()  # Вернуть данные в формате JSON
         except requests.exceptions.HTTPError as err:
             print(f"HTTP error occurred: {err}")
+        except Exception as err:
+            print(f"Other error occurred: {err}")
+        return None
+
+    def create_item(self, data):
+        """Создание нового элемента через запрос POST"""
+        try:
+            response = requests.post(f"{self.url}/item", json=data)
+            response.raise_for_status()  # Проверка на успешный ответ
+            return response.json()  # Вернуть данные в формате JSON
+        except requests.exceptions.HTTPError as err:
+            print(f"HTTP error occurred: {err}")
+        except Exception as err:
+            print(f"Other error occurred: {err}")
+        return None
 
     def update_item(self, item_id, data):
+        """Обновление элемента по ID через запрос PUT"""
         try:
             response = requests.put(f"{self.url}/item/{item_id}", json=data)
-            response.raise_for_status()
-            print('success')
-            return response.json()
+            response.raise_for_status()  # Проверка на успешный ответ
+            return response.json()  # Вернуть обновленные данные
         except requests.exceptions.HTTPError as err:
-            if response.status_code == 404:
-                print('id error')
-            else:
-                print(f"HTTP error occurred: {err}")
+            print(f"HTTP error occurred: {err}")
+        except Exception as err:
+            print(f"Other error occurred: {err}")
+        return None
--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
+from datetime import datetime
+from time import sleep
+import RPI.GPIO as GPIO
+from RPLCD.gpio import CharLCD
+
 from scanner import barcode_scanner
+from scales import getting_weight
+from constants import KEY, COLLECTION
+from button import Button, PinOUT, PinIN
+from display.display_melton import Display
+from connection.base import APIConnection
 
 
 def scanner_check() -> int:
     key = barcode_scanner()
     return key
+
+
+def scales_check() -> str:
+    global is_scale
+    weight = getting_weight()
+    if weight > 0:
+        is_scale = True
+        return weight
+    else:
+        is_scale = False
 
 
 def main() -> None:
